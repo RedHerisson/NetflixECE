af2ff662472b63f4fc9e7f8337eda5b0bd6a2599
IgoRado <igorradojicic75@gmail.com>
2023-04-15 04:22:36 +0200

    Graphs + new design
    Not linked to DC yet

--- main/com/Vue/Main.java
+++ main/com/Vue/Main.java
@@ -9,31 +9,16 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-    double x, y=0;
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("/ressources/View/adminmenu.fxml"));
-        //Parent root = FXMLLoader.load(getClass().getResource("registration.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/ressources/View/adminStats.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
-/*
-        root.setOnMousePressed(event ->{
-            x = event.getSceneX();
-            y = event.getSceneY();
-        });
-
-        root.setOnMouseDragged(event ->{
-            primaryStage.setX(event.getScreenX()-x);
-            primaryStage.setY(event.getScreenY()-y);
-        });
-*/
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
-    public static void Main(String[] args){
-        launch(args);
-    }
+//    public static void Main(String[] args){
+//        launch(args);
+//    }
 
 }
